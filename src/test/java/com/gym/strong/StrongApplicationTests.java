0594a2789de20bd149e33f01bf348a03fa304da6
dabdybapuulu@nurtelecom.kg <dabdybapuulu@nurtelecom.kg>
2023-10-12 18:01:53 +0600

    Added 1 default test

--- src/test/java/com/gym/strong/StrongApplicationTests.java
+++ src/test/java/com/gym/strong/StrongApplicationTests.java
@@ -3,11 +3,14 @@ package com.gym.strong;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class StrongApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
